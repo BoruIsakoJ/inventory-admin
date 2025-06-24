b67e7455614e763a8aaeea8f20a832a18ba60563
Boru Isako <isco.jaldy@gmail.com>
2025-06-24 17:03:10 +0300

    Add User and UserRole models with validation and password handling

---
+++ server/models.py
@@ -0,0 +1,63 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
+
+
+db = SQLAlchemy()
+
+
+class User(db.Model,SerializerMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String, nullable= False)
+    email=db.Column(db.String, nullable=False, unique=True)
+    _password_hash=db.Column(db.String, nullable=False)
+    user_role_id=db.Column(db.Integer, db.ForeignKey('user_roles.id'))
+    
+    def __repr__(self):
+        return f'<User {self.name}, {self.email}>'
+    
+    
+    validates('name')
+    def validate_name(self,key,name):
+        if not name:
+            raise ValueError('Name must not be empty')
+        return name
+    
+    @validates('email')
+    def validate_email(self,key,email):
+        if not email:
+            raise ValueError('Email must not be empty')
+        
+        if '@' not in email:
+            raise ValueError('Invalid email address. Address must have \'@\' symbol')
+        
+        return email
+    
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+    
+    @password_hash.setter
+    def password_hash(self,password):
+        from app import bcrypt
+        password_hash =bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+        
+    def authenticate(self,password):
+        from app import bcrypt
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+    
+    
+
+class UserRole(db.Model,SerializerMixin):
+    __tablename__='user_roles'
+    
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String, nullable=False)
+    
+    def __repr__(self):
+        return f'<UserRole {self.name}>'
