5cf8be80808ad174fe1fe2856960ee4e1a3d3828
Boru Isako <isco.jaldy@gmail.com>
2025-06-27 19:20:57 +0300

    Updated LandingPage layout and enhance user welcome message

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,22 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 function LandingPage() {
   return (
-    <div className="container d-flex justify-content-center align-items-center vh-100">
-      <div className="card p-5 shadow" style={{ maxWidth: "500px", width: "100%" }}>
-        <h1 className="text-center mb-4">Welcome to this React Application</h1>
-        <p className="text-center">
-          <Link to="/login" className="btn btn-primary btn-lg mx-2">
-            Login
-          </Link>
-          <Link to="/register" className="btn btn-success btn-lg mx-2">
-            Register
-          </Link>
-        </p>
+    <>
+      <nav className="navbar shadow-sm d-flex justify-content-between">
+        <div className="container">
+          <Link className="navbar-brand fw-bold text-primary" to="/">InventoryPro</Link>
+          <div >
+            <Link to="/login" className="btn btn-primary me-2">Login</Link>
+            <Link to="/register" className="btn btn-success">Register</Link>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container-fluid d-flex align-items-center" style={{ height: '100vh' }}>
+        <div className="container">
+          <div className="row">
+
+            <div className="col-md-6 d-flex flex-column justify-content-start" >
+              <h1 className="display-3 fw-bold" style={{ marginTop: '-150px' }}>
+                Manage your inventory <br /> with ease and efficiency.
+              </h1>
+              <p>
+                Track stock levels, suppliers, categories, and users — all from one dashboard.
+              </p>
+            </div>
+
+            <div className="col-md-6 d-flex flex-column justify-content-center" style={{ paddingRight: '3%', marginTop: "100px" }}>
+              <p>
+                <strong>Welcome to <span className="text-primary">InventoryPro</span></strong> your all-in-one inventory management platform.
+                <br /><br />
+                Streamlined. Secure. Smart.
+              </p>
+              <Link to="/login" className="btn btn-outline-primary btn-lg mt-3" style={{ width: '300px' }}>
+                Get Started
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    </>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
